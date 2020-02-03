aee48d3dae2e15e450087098f3f98ea5045f1789
Jeff Labonte <grimsleepless@protonmail.com>
2020-02-02 22:37:36 -0500

    First example, linear chart

---
+++ src/chapter_3/linear_chart.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from matplotlib import pyplot as plt
+
+
+years = [1950, 1960, 1970, 1980, 1990, 2000, 2010]
+gdp = [300.2, 543.3, 1075.9, 2862.5, 5979.6, 10289.7, 14958.3]
+
+
+# Create a line chart, years on x-axis and gdp on y-axis
+plt.plot(years, gdp, color="green", marker="o", linestyle="solid")
+
+plt.title("Nominal GDP")
+
+plt.ylabel("Billion of $")
+plt.show()

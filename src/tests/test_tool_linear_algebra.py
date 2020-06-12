096a16329ad95de9b369d5fbff677ab743a33c82
Jeff Labonte <jeff.labonte12@gmail.com>
2020-06-11 22:38:07 -0400

    Add failure test

--- src/tests/test_tool_linear_algebra.py
+++ src/tests/test_tool_linear_algebra.py
@@ -1,9 +1,15 @@
 from tools import linear_algebra
 
+import pytest
+
 
 class TestLinearAlgebraTools:
     EXPECTED_RESULT_ADD = [5, 7, 9]
 
-    def test_linear_algebra_add(self):
+    def test_linear_algebra_add_same_length_vector(self):
         result = linear_algebra.add(v=[1, 2, 3], w=[4, 5, 6])
         assert result == self.EXPECTED_RESULT_ADD
+
+    def test_linear_algebra_add_not_same_length(self):
+        with pytest.raises(RuntimeError):
+        	linear_algebra.add(v=[1, 2], w=[4, 5, 6])
--- src/tools/linear_algebra.py
+++ src/tools/linear_algebra.py
@@ -5,7 +5,13 @@ from typing import List
 Vector = List[float]
 
 
+
 def add (v: Vector, w: Vector) -> Vector:
-    if len(v) != len(w):
-        raise RuntimeError("Vector must be of the same length")
-    return [v_i + w_i for v_i, w_i in zip(v, w)]
+	"""
+	Add two vector two vector together
+
+	:raises: RuntimeError
+	"""
+	if len(v) != len(w):
+		raise RuntimeError("Vector must be of the same length")
+	return [v_i + w_i for v_i, w_i in zip(v, w)]
